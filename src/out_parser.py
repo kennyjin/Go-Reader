a760525741dbbb1c1c87b2deee01b2f666ace80e
kennyjin <jianianjin@gmail.com>
2019-05-19 10:16:53 -0500

    Update style

--- src/out_parser.py
+++ src/out_parser.py
@@ -1,83 +1,90 @@
 """
-This module parses Leela Zero output to a csv file containing winrate and other information.
+This module parses Leela Zero output to a csv file containing win rate and other information.
 """
 
 import re
 
-def getTextBlocks(lz_out_txt):
+
+def get_text_blocks(lz_out_txt):
     blks = re.split(r"Thinking at most", lz_out_txt)
     return blks[1:]
 
-def getWinRate(txt_blk):
-    x = re.search(r"V:\s{1,2}\d{1,3}\.\d\d%",txt_blk)
+
+def get_win_rate(txt_blk):
+    x = re.search(r"V:\s{1,2}\d{1,3}\.\d\d%", txt_blk)
     end_index = x.group().find("%")
-    winrate = (float)(x.group()[3:end_index]) # get the number part
-    return winrate
+    win_rate = float(x.group()[3:end_index])  # get the number part
+    return win_rate
 
-def getNextMove(txt_blk):
-    x = re.search(r"[A-T]{1}\d{1,2} ->", txt_blk)
+
+def get_next_move(txt_blk):
+    x = re.search(r"[A-T]\d{1,2} ->", txt_blk)
     end_index = x.group().find(" ")
-    nextMove = x.group()[0:end_index]
-    return nextMove
+    next_move = x.group()[0:end_index]
+    return next_move
 
-def getVariation(txt_blk):
+
+def get_variation(txt_blk):
     x = re.search(r"PV: .*\n", txt_blk)
     variation = x.group()[4:-1]
     return variation
 
-def readLZout(lz_out_file):
+
+def read_lz_out(lz_out_file):
     f = open(lz_out_file, "r")
     lz_out_txt = f.read()
     f.close()
     return lz_out_txt
 
-def printData(lz_out_file, showNextMove = True, showWinRate = True, showVariation = False):
-    lz_out_txt = readLZout(lz_out_file)
-    blks = getTextBlocks(lz_out_txt)
+
+def print_data(lz_out_file, show_next_move=True, show_win_rate=True, show_variation=False):
+    lz_out_txt = read_lz_out(lz_out_file)
+    blks = get_text_blocks(lz_out_txt)
     print(len(blks))
     for i in range(len(blks)):
         outstr = ""
-        if showNextMove:
-            outstr += getNextMove(blks[i])
-        if showWinRate:
-            outstr += " " + (str)(getWinRate(blks[i]))
-        if showVariation:
-            outstr += " " + getVariation(blks[i])
+        if show_next_move:
+            outstr += get_next_move(blks[i])
+        if show_win_rate:
+            outstr += " " + "{0:.2f}".format(get_win_rate(blks[i]))
+        if show_variation:
+            outstr += " " + get_variation(blks[i])
         print(outstr)
 
 
-def readActualMoves(lz_cmd_file):
+def read_actual_moves(lz_cmd_file):
     f = open(lz_cmd_file, "r")
     lz_cmd_txt = f.read()
     f.close()
-    move_list = re.findall(r"play [BW]{1} [A-T]{1}\d{1,2}", lz_cmd_txt)
+    move_list = re.findall(r"play [BW] [A-T]\d{1,2}", lz_cmd_txt)
     color_list = []
     pos_list = []
     for i in range(len(move_list)):
-      color_list.append(move_list[i][5])
-      pos_list.append(move_list[i][7:])
+        color_list.append(move_list[i][5])
+        pos_list.append(move_list[i][7:])
     
     return color_list, pos_list
 
 
-# TODO keep 2 decimal places
-def printWinRate(lz_cmd_file, lz_out_file, Black = True):
-    color_list, pos_list = readActualMoves(lz_cmd_file)
-    lz_out_txt = readLZout(lz_out_file)
-    blks = getTextBlocks(lz_out_txt)
+def print_win_rate(lz_cmd_file, lz_out_file, black=True, show_move_number=True):
+    color_list, pos_list = read_actual_moves(lz_cmd_file)
+    lz_out_txt = read_lz_out(lz_out_file)
+    blks = get_text_blocks(lz_out_txt)
     print(len(blks))
     for i in range(len(blks)):
-        outstr = ""
-        if Black:
+        if black:
             if color_list[i] == "B":
-                outstr += " " + (str)(getWinRate(blks[i]))
+                # Keep 2 decimal places
+                outstr = "{0:.2f}".format(get_win_rate(blks[i]))
             else:
-                outstr += " " + (str)(100 - getWinRate(blks[i]))
+                outstr = "{0:.2f}".format(100.00 - get_win_rate(blks[i]))
         else:
             if color_list[i] == "W":
-                outstr += " " + (str)(getWinRate(blks[i]))
+                outstr = "{0:.2f}".format(get_win_rate(blks[i]))
             else:
-                outstr += " " + (str)(100 - getWinRate(blks[i]))
+                outstr = "{0:.2f}".format(100.00 - get_win_rate(blks[i]))
+        if show_move_number:
+            outstr = str(i) + " " + outstr
         print(outstr)
 
 
