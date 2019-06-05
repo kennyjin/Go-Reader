bbc2a1b19b2b0e933f85199e31d683dc50765942
kennyjin <jianianjin@gmail.com>
2019-06-05 14:00:14 -0500

    Implemented basic sgf generation

--- src/image_parser.py
+++ src/image_parser.py
@@ -3,8 +3,6 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-
-
 """
 Get the color of the stone on the specific square.
 Could be black stone, white stone or no stone.
@@ -13,8 +11,8 @@ TODO This function definitely needs further improvements.
 """
 
 
-def get_stone_color(img):
-    mean_color = np.mean(img)
+def get_stone_color(square):
+    mean_color = np.mean(square)
     if mean_color >= 200:
         return -1
     if mean_color <= 90:
@@ -23,71 +21,127 @@ def get_stone_color(img):
 
 
 """
+Get game position a Go board image, return an 2d array of numbers(-1, 0 or 1).
+Each square could contain black stone, white stone or no stone.
+"""
+
+
+def get_game_position(go_board, edge_len_x, edge_len_y, stone_len, board_size=19):
+    rows = cols = board_size
+    pos_array = [[0 for i in range(cols)] for j in range(rows)]
+    for y in range(board_size):
+        for x in range(board_size):
+            piece = go_board[edge_len_y + stone_len * y:edge_len_y + stone_len * (y + 1),
+                             edge_len_x + stone_len * x:edge_len_x + stone_len * (x + 1)]
+            pos_array[y][x] = get_stone_color(piece)
+            # print(square_array[y])
+            # cv2.imshow('image', piece)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+    return pos_array
+
+
+"""
+Get Go board from an image.
+"""
+
+
+def get_go_board(game_image):
+    return None
+
+
+
+"""
+Generate sgf string according to the game position array.
+"""
+
+
+def get_sgf_txt(pos_array, board_size=19):
+    black_pos = []
+    white_pos = []
+    sgf_txt = "(;"
+    for i in range(board_size):
+        for j in range(board_size):
+            if pos_array[i][j] == 1:
+                black_pos.append(num_to_letter(j)+num_to_letter(i))
+            if pos_array[i][j] == -1:
+                white_pos.append(num_to_letter(j)+num_to_letter(i))
+    sgf_txt += "AB"
+    for pos in black_pos:
+        sgf_txt += "[" + pos + "]"
+    sgf_txt += "AW"
+    for pos in white_pos:
+        sgf_txt += "[" + pos + "]"
+    sgf_txt += ")"
+    return sgf_txt
+
+
+"""
+Transform coordinate(number) to coordinate(letter).
+Number should be from 0-18, letter should be a-s.
+"""
+
+
+def num_to_letter(num):
+    diff = ord('a') - 0
+    return chr(num + diff)
+
+
+
+"""
+Convert Go game image to an sgf file.
+"""
+
+
+def img_to_sgf(game_image, sgf_file):
+    return None
+
+
+"""
 Show RGB histogram of an image.
 """
 
 
-def plot_histogram(img):
+def plot_histogram(square):
     color = ('b', 'g', 'r')
     for k, col in enumerate(color):
-        hist = cv2.calcHist([img], [k], None, [256], [0, 256])
+        hist = cv2.calcHist([square], [k], None, [256], [0, 256])
         plt.plot(hist, color=col)
         plt.xlim([0, 256])
     plt.show()
 
 
 img = cv2.imread('../images/13.PNG')
-# cv2.imshow('image', img)
-# cv2.waitKey(0)  # Need this other wise the window will not respond
-# cv2.destroyAllWindows()
-# print(img.item(10, 10))
-print(img.shape)
 
 
-# length_square = float(img.shape[0]) / 20
-# print(length_square)
-# # img_list = []
-# # for i in range(1, 21):
-# #     img_list.append(img[int(length_square * (i-1)): int(length_square * i), 0:int(length_square)])
-# cv2.imshow('image', img[1:90, 0:76*20])
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-mean_color_list = np.array([])
-
-for i in range(19):
-    for j in range(19):
-        # ret, piece = cv2.threshold(img[79 * i:79 * (i + 1), 79 * j:79 * (j + 1)], 75, 255, cv2.THRESH_BINARY)
-        # cv2.imshow('image', piece)
-        piece = img[49 + 94 * i:49 + 94 * (i + 1), 49 + 94 * j:49 + 94 * (j + 1)]
-        # print(img[49 + 94 * i:49 + 94 * (i + 1), 49 + 94 * j:49 + 94 * (j + 1)].shape)
-        # cv2.imshow('image', img[49 + 94 * j:49 + 94 * (j + 1), 49 + 94 * i:49 + 94 * (i + 1)])
-        # print(img[79 * i:79 * (i + 1), 79 * j:79 * (j + 1)])
-        # print(np.mean(piece))
-        print(str(get_stone_color(piece)))
-        # mean_color_list = np.append(mean_color_list, [np.mean(piece)])
-        # print(img[79 * i:79 * (i + 1), 79 * j:79 * (j + 1)].var())
-        cv2.imshow('image', piece)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+# mean_color_list = np.array([])
+#
+# for i in range(19):
+#     for j in range(19):
+#         # ret, piece = cv2.threshold(img[79 * i:79 * (i + 1), 79 * j:79 * (j + 1)], 75, 255, cv2.THRESH_BINARY)
+#         # cv2.imshow('image', piece)
+#         piece = img[49 + 94 * i:49 + 94 * (i + 1), 49 + 94 * j:49 + 94 * (j + 1)]
+#         # print(img[49 + 94 * i:49 + 94 * (i + 1), 49 + 94 * j:49 + 94 * (j + 1)].shape)
+#         # cv2.imshow('image', img[49 + 94 * j:49 + 94 * (j + 1), 49 + 94 * i:49 + 94 * (i + 1)])
+#         # print(img[79 * i:79 * (i + 1), 79 * j:79 * (j + 1)])
+#         # print(np.mean(piece))
+#         print(str(get_stone_color(piece)))
+#         # mean_color_list = np.append(mean_color_list, [np.mean(piece)])
+#         # print(img[79 * i:79 * (i + 1), 79 * j:79 * (j + 1)].var())
+#         cv2.imshow('image', piece)
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
 # print(mean_color_list)
 # plt.hist(mean_color_list, bins=50)
 # plt.show()
 
 
+game_position = get_game_position(img, 49, 49, 94)
 
 
-
-
-
-
-
-
-
-
-
-
+f = open("out1.sgf", "w")
+f.write(get_sgf_txt(game_position))
+f.close()
 
 
 
